89715da68463a740ae1e0a5db9d7c5516b988882
haxvendas <thiagohax9@gmail.com>
2025-08-21 18:58:12 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hax-vendas-v1.2.9'; // Atualize a versão quando quiser forçar atualização
+const CACHE_NAME = 'hax-vendas-v1.3.0'; // Atualize a versão quando quiser forçar atualização
 const urlsToCache = [
   '/',
   '/index.html',
